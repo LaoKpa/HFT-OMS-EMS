0e335eca32bd39891bc2c03671b61976c0e55331
Anish <anishmirjankar@gmail.com>
2018-11-08 02:21:49 -0500

    Initializing driver with runtime

---
+++ src/driver.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <ctime>
+
+/**
+ * Application driver
+ * TODO:
+ * Compile automatically outside of folder 
+ */
+
+using namespace std;
+
+int main()
+{
+    cout << "Starting app... \n";
+
+    clock_t t_start = clock();
+
+    /**
+     * Run all ops here + runtime to beginning
+     * TODO:
+     * 1) Connect to InteractiveBrokers API
+     * 2) Run trading test suite
+     * 3) Analytics, etc. 
+     */
+
+
+    auto output = "Runtime: " + to_string((clock() - t_start)/(double)CLOCKS_PER_SEC) + "\n";
+    cout << output;
+}
